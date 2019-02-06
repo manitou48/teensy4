3f11bd108812018638d293cc272789021aa966e2
manitou48 <manitou@home>
2019-02-06 07:34:07 -0500

    format

--- cachetst/cachetst.ino
+++ cachetst/cachetst.ino
@@ -1,9 +1,11 @@
 // disableDCache() based on SDK SCB_DisableDCache()
 // 32KB D and I cache
-// no cache data in DTCM,  OCRAM (malloc()) and FLASH (PROGRMEM RO?) cache data
+// no cache data in DTCM,  OCRAM (malloc()) and FLASH (PROGMEM RO) cache data
 #include "pg.h"
+
 #define REPS 1000
 #define XN 1000
+//PROGMEM float ap[XN], bp[XN];
 
 #define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
 
@@ -29,7 +31,7 @@ void disableDCache() {
 
 }
 
-float  sdot(float *a, float *b, int n) {
+float  sdot(float *a, float *b, int n, char *memlbl) {
   float sum = 0;
 
   uint32_t t = micros();
@@ -37,7 +39,7 @@ float  sdot(float *a, float *b, int n) {
     for (int i = 0; i < n; i++) sum += a[i] * b[i];
   }
   t = micros() - t;
-  Serial.printf("%d us\n", t);
+  Serial.printf("%-15s 0x%0X  %8d  %6.1f %.0f\n", memlbl, (uint32_t)a, t, 2.*n * REPS / t, sum);
   return sum;
 }
 
@@ -45,25 +47,28 @@ float  sdot(float *a, float *b, int n) {
 void setup() {
   float a[XN], b[XN], r, *am, *bm; // more than 32KB
 
-  disableDCache();  // or not
+
   Serial.begin(9600);
   while (!Serial);
   delay(1000);
   am = (float *) malloc(XN * 4);
   bm = (float *) malloc(XN * 4);
-  Serial.printf("stack 0x%x  malloc 0x%x PROGMEM 0x%x\n",
-                (uint32_t)a, (uint32_t) am, (uint32_t) ap);
   for (int i = 0; i < XN; i++) a[i] = b[i] = am[i] = bm[i] = ap[i];
   PRREG(SCB_CCR);
   PRREG(SCB_ID_CCSIDR);
   PRREG(SCB_ID_CSSELR);
   PRREG(SCB_ID_CLIDR);
-  r = sdot(a, b, XN);
-  Serial.println(r);
-  r = sdot(ap, bp, XN);
-  Serial.println(r);
-  r = sdot(am, bm, XN);
-  Serial.println(r);
+  Serial.printf("N %d  REPS %d\n", XN, REPS);
+  Serial.printf("memory              addr        us    mflops  sum\n");
+  sdot(a, b, XN, "stack/DTCM");
+  sdot(ap, bp, XN, "malloc/OCRAM");
+  sdot(am, bm, XN, "PROGMEM/flash");
+  disableDCache();  // or not
+  Serial.printf("D cache off\n");
+  sdot(a, b, XN, "stack/DTCM");
+  sdot(ap, bp, XN, "malloc/OCRAM");
+  sdot(am, bm, XN, "PROGMEM/flash");
+  PRREG(SCB_CCR);
 }
 
 void loop() {
