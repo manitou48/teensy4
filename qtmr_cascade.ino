8d08df00d81d428956758e108aa456eec715017b
manitou48 <manitou@home>
2019-07-18 06:17:26 -0400

    edit chain

--- qtmr_cascade.ino
+++ qtmr_cascade.ino
@@ -1,16 +1,16 @@
-// QTIMER4 cascade  test   F_BUS_ACTUAL 150 mhz, 50%duty  ch 53
-// 16-bit counter, chain with 3 channels, count us for up to 65 seconds
+// QTIMER4 cascade  test   F_BUS_ACTUAL 150 mhz, 50% duty  ch 53
+// 16-bit counter, chain with 3 channels, count us
 
 #define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
 
 #define IRQ_QTIMERx IRQ_QTIMER4
-#define CHn 1
+
 IMXRT_TMR_t * TMRx = (IMXRT_TMR_t *)&IMXRT_TMR4;
 
 
 volatile uint32_t ticks, isrms;
 void my_isr() {
-  TMRx->CH[CHn].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
+  TMRx->CH[1].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
   ticks++;
   isrms = millis();
   asm volatile ("dsb");  // wait for clear  memory barrier
@@ -25,37 +25,37 @@ void isr_init(int hz) {
     hz *= 2;
     cnt = 150000000 / hz;
   }
-  TMRx->CH[CHn].CTRL = 0; // stop
-  TMRx->CH[CHn].LOAD = 0;  // start val after compare
-  TMRx->CH[CHn].COMP1 = cnt - 1;  // count up to this val, interrupt,  and start again
-  TMRx->CH[CHn].CMPLD1 = cnt - 1;
-  TMRx->CH[CHn].CTRL = TMR_CTRL_CM(1) | TMR_CTRL_PCS(8 + pcs) | TMR_CTRL_LENGTH ;  // prescale
+  TMRx->CH[1].CTRL = 0; // stop
+  TMRx->CH[1].LOAD = 0;  // start val after compare
+  TMRx->CH[1].COMP1 = cnt - 1;  // count up to this val, interrupt,  and start again
+  TMRx->CH[1].CMPLD1 = cnt - 1;
+  TMRx->CH[1].CTRL = TMR_CTRL_CM(1) | TMR_CTRL_PCS(8 + pcs) | TMR_CTRL_LENGTH ;  // prescale
   attachInterruptVector(IRQ_QTIMERx, my_isr);
-  TMRx->CH[CHn].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
-  TMRx->CH[CHn].CSCTRL |= TMR_CSCTRL_TCF1EN;  // enable interrupt
+  TMRx->CH[1].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
+  TMRx->CH[1].CSCTRL |= TMR_CSCTRL_TCF1EN;  // enable interrupt
   NVIC_ENABLE_IRQ(IRQ_QTIMERx);
 }
 
 void cascade() {
-  // CH0 tick every 1 ms, clock 150mhz/128  1171875 hz, comp 1172-1 ish
+  // CH0 tick every 1 ms, clock 150mhz/128  1171875 hz, comp 1172 ish
   // CH1 clocked from CH0 ticks, interrupt every 5000 ms
   int cnt;
 
-  cnt = 1173 - 1; // 1 ms
+  cnt = 1172; // 1 ms
   TMRx->CH[0].CTRL = 0; // stop
   TMRx->CH[0].LOAD = 0;  // start val after compare
   TMRx->CH[0].COMP1 = cnt - 1;  // count up to this val and start again
   TMRx->CH[0].CMPLD1 = cnt - 1;
   TMRx->CH[0].CTRL = TMR_CTRL_CM(1) | TMR_CTRL_PCS(8 + 7) | TMR_CTRL_LENGTH ;  // /128
-  cnt = 5000 - 1; //  5000 ms
-  TMRx->CH[CHn].CTRL = 0; // stop
-  TMRx->CH[CHn].LOAD = 0;  // start val after compare
-  TMRx->CH[CHn].COMP1 = cnt - 1;  // count up to this val, interrupt,  and start again
-  TMRx->CH[CHn].CMPLD1 = cnt - 1;
-  TMRx->CH[CHn].CTRL = TMR_CTRL_CM(7) | TMR_CTRL_PCS(4) | TMR_CTRL_LENGTH ;  //clock from clock 0
+  cnt = 5000 ; //  5000 ms
+  TMRx->CH[1].CTRL = 0; // stop
+  TMRx->CH[1].LOAD = 0;  // start val after compare
+  TMRx->CH[1].COMP1 = cnt - 1;  // count up to this val, interrupt,  and start again
+  TMRx->CH[1].CMPLD1 = cnt - 1;
+  TMRx->CH[1].CTRL = TMR_CTRL_CM(7) | TMR_CTRL_PCS(4) | TMR_CTRL_LENGTH ;  //clock from clock 0
   attachInterruptVector(IRQ_QTIMERx, my_isr);
-  TMRx->CH[CHn].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
-  TMRx->CH[CHn].CSCTRL |= TMR_CSCTRL_TCF1EN;  // enable interrupt
+  TMRx->CH[1].CSCTRL &= ~(TMR_CSCTRL_TCF1);  // clear
+  TMRx->CH[1].CSCTRL |= TMR_CSCTRL_TCF1EN;  // enable interrupt
   NVIC_ENABLE_IRQ(IRQ_QTIMERx);
 }
 
@@ -66,16 +66,17 @@ void chain() {
 
   int cnt;
 
-  cnt = 150 - 1; // 1 us
+  cnt = 150 ; // 1 us
   TMRx->CH[0].CTRL = 0; // stop
+  TMRx->CH[0].CNTR = 0;
   TMRx->CH[0].LOAD = 0;  // start val after compare
   TMRx->CH[0].COMP1 = cnt - 1;  // count up to this val and start again
-  TMRx->CH[0].CMPLD1 = cnt - 1;
+  TMRx->CH[0].CMPLD1 = cnt - 1  ;
   TMRx->CH[0].CTRL = TMR_CTRL_CM(1) | TMR_CTRL_PCS(8 ) | TMR_CTRL_LENGTH ;  // no prescale
-  cnt = 1000; //  count 1000 us
+  cnt = 65536;
   TMRx->CH[1].CTRL = 0; // stop
   TMRx->CH[1].LOAD = 0;  // start val after compare
-  TMRx->CH[1].COMP1 = cnt - 1 ;
+  TMRx->CH[1].COMP1 = cnt - 1;
   TMRx->CH[1].CMPLD1 = cnt - 1;
   TMRx->CH[1].CTRL = TMR_CTRL_CM(7) | TMR_CTRL_PCS(4) | TMR_CTRL_LENGTH ;  //clock from clock 0
   cnt = 0; //  count ms
@@ -94,26 +95,38 @@ void setup()   {
   CCM_CCGR6 |= CCM_CCGR6_QTIMER1(CCM_CCGR_ON);
 
   // pick a test
-  // cascade();
+  //  cascade();
   chain();
   // isr_init(20000);   // hz
 
   PRREG(TMRx->CH[0].CTRL);
   PRREG(TMRx->CH[0].COMP1);
 
-  PRREG(TMRx->CH[CHn].CTRL);
-  PRREG(TMRx->CH[CHn].SCTRL);
-  PRREG(TMRx->CH[CHn].CSCTRL);
-  PRREG(TMRx->CH[CHn].LOAD);
-  PRREG(TMRx->CH[CHn].COMP1);
-  PRREG(TMRx->CH[CHn].CMPLD1);
-  PRREG(TMRx->CH[CHn].COMP2);
-  PRREG(TMRx->CH[CHn].CMPLD2);
+  PRREG(TMRx->CH[1].CTRL);
+  PRREG(TMRx->CH[1].SCTRL);
+  PRREG(TMRx->CH[1].CSCTRL);
+  PRREG(TMRx->CH[1].LOAD);
+  PRREG(TMRx->CH[1].COMP1);
+  PRREG(TMRx->CH[1].CMPLD1);
+  PRREG(TMRx->CH[1].COMP2);
+  PRREG(TMRx->CH[1].CMPLD2);
+  // chain test
+  uint32_t us = TMRx->CH[1].CNTR + 65536 * TMRx->CH[2].HOLD;
+  delay(10);
+  uint32_t us1 = TMRx->CH[1].CNTR + 65536 * TMRx->CH[2].HOLD;
+  Serial.println(us1 - us);
+  us = micros();
+  delay(10);
+  us1 = micros();
+  Serial.println(us1 - us);
 }
 
+uint32_t prev = micros();
 void loop()
 {
-  uint32_t us = TMRx->CH[1].CNTR + 1000 * TMRx->CH[2].HOLD; // could count second chnl 3
-  Serial.printf("%d ticks %d ms   %d us\n", ticks, isrms, us);
-  delay(1000);
+  if (micros() - prev > 1000000) {
+    uint32_t us = TMRx->CH[1].CNTR + 65536 * TMRx->CH[2].HOLD; // could count second chnl 3
+    Serial.printf("%d ticks %d ms   %d us %d\n", ticks, isrms, us, micros());
+    prev = micros();
+  }
 }
