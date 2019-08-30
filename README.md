7e3d99c00fd230743900b929b360fd58e35cb8df
manitou48 <manitou@home>
2019-08-30 13:51:04 -0400

    PIT 64-bit micros

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ gpt_count.ino   | GPT1 clocked from pin 25, like FreqCount
 gpt_isr.ino  |  GPT2 compare interrupt
 gpt_micros.ino |  GPT1 micros (core micros only 10 us res)
 gpt_pwm.ino | GPT2 @150mhz PWM to pin 16
+pit_micros64.ino | free running PIT timer 64-bit microseconds
 pitxbaradc.ino |  clock ADC reads with PIT via XBAR
 qtmr_capture.ino |quad timer capture
 qtmr_cascade.ino | cascade or chain quad timer channels
---
+++ pit_micros64.ino
@@ -0,0 +1,41 @@
+// PIT timer free running  24mhz clock  64-bit cycle count
+// chain chn 1 to chn 0
+//  teensy4 uses PIT for IntervalTimer
+
+#define PRREG(x) Serial.printf(#x" 0x%x\n",x);
+
+void pit_init()
+{
+  CCM_CCGR1 |= CCM_CCGR1_PIT(CCM_CCGR_ON);
+
+  PIT_MCR = 0;
+
+  IMXRT_PIT_CHANNELS[1].LDVAL = 0xffffffff;   // max
+  IMXRT_PIT_CHANNELS[1].TFLG = 1;        // clear
+  IMXRT_PIT_CHANNELS[1].TCTRL = PIT_TCTRL_TEN | PIT_TCTRL_CHN; // chain to 0
+
+  IMXRT_PIT_CHANNELS[0].LDVAL = 0xffffffff;
+  IMXRT_PIT_CHANNELS[0].TFLG = 1;
+  IMXRT_PIT_CHANNELS[0].TCTRL = PIT_TCTRL_TEN;
+}
+
+uint64_t pit_cycles() {
+  uint64_t valueH;
+  volatile uint32_t valueL;
+  valueH = PIT_LTMR64H;   //  no rollover
+  valueL = PIT_LTMR64L;
+  return ~((valueH << 32) | valueL);   // invert for up counting
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);
+  pit_init();
+}
+
+void loop()
+{
+  Serial.printf("%lld us\n", pit_cycles() / 24);
+  delay(1000);
+}
