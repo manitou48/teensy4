76fd7d2a3b40ad7181f1eb4250b52c68e85c4392
manitou48 <manitou@home>
2020-07-30 15:55:51 -0400

    GPT @150mhz

--- gpsgpt.ino
+++ gpsgpt.ino
@@ -1,11 +1,13 @@
 // GPS PPS on pin 12  GPIO2 1  B0_01  time with GPT1
 // clock(1) 24mhz   clock(4) 32khz   set  CLKSRC
 // clock(5) doesn't tick
+// 24mhz  PREDIV 0 TPS 24000000,  PREDIV 5  TPS 4000000
+// @150 mhz PREDIV 0 TPS 150000000 to get 150 mhz
 
 #define CLKSRC 1     // 1 or 4
 #if CLKSRC == 1
-#define TPS 4000000
-#define PREDIV 5
+#define TPS 150000000
+#define PREDIV 0
 #elif CLKSRC == 4
 #define TPS 32768
 #define PREDIV 0
@@ -28,6 +30,8 @@ void setup() {
   while (!Serial);
   delay(3000);
   Serial.println("starting");
+  // uncomment following for 150mhz
+  CCM_CSCMR1 &= ~CCM_CSCMR1_PERCLK_CLK_SEL; // turn off 24mhz mode
   CCM_CCGR1 |= CCM_CCGR1_GPT(CCM_CCGR_ON) ;  // enable GPT1 module
   GPT1_CR = 0;
   GPT1_PR = PREDIV;   // prescale+1 /1 for 32k,  /24 for 24mhz   /24 clock 1
@@ -45,7 +49,7 @@ void loop() {
       int d = ticks - prev;
       float ppm = 1000000. * (d - TPS) / TPS;
       sum += ppm;
-      Serial.printf("%d secs %d ticks  %.2f  %.2f ppm  %.1f C\n", n, d, ppm, sum / n, tempmonGetTemp());
+      Serial.printf("%d secs %d ticks  %.3f  %.3f ppm  %.1f C\n", n, d, ppm, sum / n, tempmonGetTemp());
       n++;
     }
     tick = 0;
